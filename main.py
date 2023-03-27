bf7588592af56755b082323e36e9b07d5907a28e
sumit20136 <89103566+sumit20136@users.noreply.github.com>
2023-03-27 23:57:50 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,116 +1,138 @@
 import streamlit as st
+import requests
+import time
+import moviepy.editor as mp
+from io import StringIO
+from PIL import Image
+import os 
 
-# Set page title
+
+@st.cache_data
+def process_video(uploaded_file):
+    clip = mp.VideoFileClip(uploaded_file.name) 
+    clip.audio.write_audiofile(r"converted.wav")
+    UPLOAD_ENDPOINT = "https://api.assemblyai.com/v2/upload"
+    TRANSCRIPTION_ENDPOINT = "https://api.assemblyai.com/v2/transcript"
+    api_key = "dc737c08289441578a2f68b04d404f74"
+    headers = {"authorization": api_key, "content-type": "application/json"}
+
+    def read_file(filename):
+       with open(filename, 'rb') as _file:
+           while True:
+               data = _file.read(5242880)
+               if not data:
+                   break
+               yield data
+
+    upload_response = requests.post(UPLOAD_ENDPOINT, headers=headers, data=read_file('converted.wav'))
+    audio_url = upload_response.json()["upload_url"]
+    transcript_request = {'audio_url': audio_url}
+    transcript_response = requests.post(TRANSCRIPTION_ENDPOINT, json=transcript_request, headers=headers)
+    _id = transcript_response.json()["id"]
+
+    while True:
+        polling_response = requests.get(TRANSCRIPTION_ENDPOINT + "/" + _id, headers=headers)
+        if polling_response.json()['status'] == 'completed':
+            with open(f'{_id}.txt', 'w') as f:
+                f.write(polling_response.json()['text'])
+            print('Transcript saved to', _id, '.txt')
+            break
+
+        elif polling_response.json()['status'] == 'error':
+            raise Exception("Transcription failed. Make sure a valid API key has been used.")
+        else:
+            print("Transcription queued or processing ...")
+            # time.sleep(5)    
+    return
+
+
+# @st.cache_data
+# def change_text(data):
+#     data=data.lower()
+#     data=nltk.word_tokenize(data)
+#     english_stopwords = stopwords.words()
+#     tokens_wo_stopwords = [t for t in data if t not in english_stopwords]
+#     data=tokens_wo_stopwords
+#     lst=[]
+#     for i in data:
+#         if(i.isalpha()):
+#             lst.append(i)   
+#     data= lst
+#     return " ".join(data)    
+
+# @st.cache_data
+# def analyse_text():
+#     file_name=""
+#     data=""
+#     for file in os.listdir(os.getcwd()):
+#         if file.endswith(".txt"):
+#             file_name=file
+#             # file_name=file_name+".txt"
+#             # print(file_name)
+#             with open(file_name, 'r') as file:
+#                 data = file.read() 
+    
+    # final_data=change_text(data)
+    # word_cloud = WordCloud(collocations = False, background_color = 'white').generate(final_data)
+    # # Display the generated Word Cloud
+    # plt.imshow(word_cloud, interpolation='bilinear')
+    # plt.axis("off")
+    # plt.show()
+
+@st.cache_data
+def print_plots():
+    dependency_plot = Image.open('dependency_plot.jpg')
+    image_waveform = Image.open("image_waveform.png")
+    wordcloud = Image.open("wordcloud.png")
+    st.image(dependency_plot, caption='Dependency Plot')
+    st.image(image_waveform, caption="Image Waveform")
+    st.image(wordcloud, caption="WordCloud")
+    
+    # analyse_text()
+    # data=data.lower()
+    # data=nltk.word_tokenize(data)
+    # english_stopwords = stopwords.words()
+    # tokens_wo_stopwords = [t for t in data if t not in english_stopwords]
+    # data=tokens_wo_stopwords
+    # lst=[]
+    # for i in data:
+    #     if(i.isalpha()):
+    #         lst.append(i)   
+    # data= lst
+    # return " ".join(data) 
+
+@st.cache_data
+def analyse_audio():
+    audio_file = open('converted.wav','rb') #enter the filename with filepath
+    audio_bytes = audio_file.read() #reading the file
+    st.audio(audio_bytes, format='audio/ogg') #displaying the audio
+    
 st.set_page_config(page_title="Emoture - An Emotion Recognition App")
+st.title("Emoture - An Emotion Recognition App")
+st.write("---")
+st.sidebar.title("Navigation")
 
-# Create a container to hold the navigation bar
-nav_container = st.container()
+            
+uploaded_file=st.sidebar.file_uploader("Upload your file here")
 
-# Add a row to the container with two columns
-cols = nav_container.columns([2, 10])
+if uploaded_file:
+    process_video(uploaded_file)
+    
+options=st.sidebar.radio("Page To Navigate", options=["Home", "Plots", "Audio"])      
 
-# Add content to the left column
-with cols[0]:
-    st.image('C:/Users/Rahul Maddula/PycharmProjects/OCH/images/OCH_Logo.png', width=50)
-
-# Add content to the right column
-with cols[1]:
-    st.title("My Awesome Streamlit App")
-
-    # Add a horizontal line for separation
-    st.write("---")
-
-    # Add links to the navigation bar
-    st.write("[Home](#)")
-    st.write("[About](#)")
-    st.write("[Contact](#)")
-
-# Add a banner image
-st.image("homepageImage.jpeg", use_column_width=True, caption="Image Source: Unsplash")
-
-# Add some content sections
-st.markdown("<h2 id='about'>About</h2>", unsafe_allow_html=True)
-st.write("""
-    This app is a simple user interface for our IR Project - An Integrated Approach to Context-aware Emotion Recognition through Posture and Speech Modulation Analysis. 
-    Through our ML models, one's emotional state can be inferred by analysing their posture and speech modulation, while also being contextually aware.
-""")
-
-st.markdown("<h2 id='upload'>Upload File</h2>", unsafe_allow_html=True)
-uploaded_file = st.file_uploader("Choose an MP4 video file to upload.", type="mp4")
-if uploaded_file is not None:
-    st.video(uploaded_file)
-
-st.markdown("<h2 id='contact'>Contact</h2>", unsafe_allow_html=True)
-st.write("""
-    If you have any questions or feedback about the app, please send an email to example@example.com.
-""")
-
-
-# Set up navigation bar
-st.write("""
-    <style>
-        .stApp {
-            background-color: #f5f5f5;
-        }
-        .stApp header {
-            position: fixed;
-            top: 0;
-            left: 0;
-            right: 0;
-            background-color: #333333;
-            color: #ffffff;
-            padding: 0.5rem;
-        }
-        .stApp header a {
-            color: #ffffff;
-            text-decoration: none;
-            margin-right: 1rem;
-        }
-        .stApp header a:hover {
-            text-decoration: underline;
-        }
-        .content-section {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-between;
-        }
-        .content-box {
-            background-color: #ffffff;
-            border: 1px solid #e1e1e1;
-            border-radius: 5px;
-            padding: 1.5rem;
-            margin-bottom: 1.5rem;
-            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-            width: calc(33.33% - 1rem);
-        }
-        .content-box img {
-            width: 100%;
-            margin-bottom: 1rem;
-        }
-        .content-box h3 {
-            margin-top: 0;
-        }
-    </style>
-""", unsafe_allow_html=True)
-st.write("<header><a href='#'>Home</a><a href='#'>Upload Video</a></header>", unsafe_allow_html=True)
-
-# Set up sidebar menu
-st.sidebar.title("Menu")
-menu_options = ["Home", "Upload Video"]
-menu_choice = st.sidebar.selectbox("Select an option", menu_options)
-
-# Display appropriate page based on menu choice
-if menu_choice == "Home":
-    # Add a banner image
-    st.image("homepageImage.jpeg", use_column_width=True)
-
-    # Add some content sections
+if options=="Home":
+    st.header("Home")
+    st.image("image_1.jpg", use_column_width=True)
+    st.markdown("<h2 id='about'>About</h2>", unsafe_allow_html=True)
+    st.write("""
+        This app is a simple user interface for our IR Project - An Integrated Approach to Context-aware Emotion Recognition through Posture and Speech Modulation Analysis. 
+        Through our ML models, one's emotional state can be inferred by analysing their posture and speech modulation, while also being contextually aware.
+    """)
     st.markdown("## What is this app?")
     st.write("""
         This app is a simple user interface for our IR Project - An Integrated Approach to Context-aware Emotion Recognition through Posture and Speech Modulation Analysis. 
         Through our ML models, one's emotional state can be inferred by analysing their posture and speech modulation, while also being contextually aware.
     """)
-
     st.markdown("## Why use this app?")
     st.write("""
         There are several benefits to using this app to play your videos:
@@ -118,7 +140,6 @@ if menu_choice == "Home":
         - Works on any device with a web browser
         - Plug and play, easy and quick to use.
     """)
-
     st.markdown("## How to use this app?")
     st.write("""
         1. Select 'Upload Video' from the menu.
@@ -126,72 +147,23 @@ if menu_choice == "Home":
         3. Wait for the video to upload and process (this may take a while for large files).
         4. Wait for the processing and view your results!
         """)
-
-    # Add some featured content boxes
-    st.markdown("## Featured Videos")
-    st.write("<div class='content-section'>", unsafe_allow_html=True)
-
+    st.markdown("<h2 id='contact'>Contact</h2>", unsafe_allow_html=True)
     st.write("""
-        <div class='content-box'>
-            <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fthenextweb.com%2Fnews%2Femotion-recognition-ai-cant-determine-how-people-feel&psig=AOvVaw2K9FFgSV-U0qzspBYvhtPj&ust=1679770357870000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLC5ia2e9f0CFQAAAAAdAAAAABAD' alt='Video 1'>
-            <h3>Video 1</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                <a href='#'>Watch Now</a>
-            </div>
-        """, unsafe_allow_html=True)
+        If you have any questions or feedback about the app, please send an email to sumit20136@iiitd.ac.in.
+    """)
+if options=="Plots":
+    if(uploaded_file is None):
+        st.header("Not uploaded")
+    else:
+        st.header("Uploaded")
+        print_plots()
+    
+elif options=="Audio":
+    if(uploaded_file is None):
+        st.header("Not uploaded")
+    else:
+        st.header("Uploaded")
+        analyse_audio()
+        
 
-    st.write("""
-            <div class='content-box'>
-                <img src='https://images.unsplash.com/photo-1541743863329-0ca247c8a9cf' alt='Video 2'>
-                <h3>Video 2</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                <a href='#'>Watch Now</a>
-            </div>
-        """, unsafe_allow_html=True)
-
-    st.write("""
-            <div class='content-box'>
-                <img src='https://images.unsplash.com/photo-1536811051087-4dbbe968c19d' alt='Video 3'>
-                <h3>Video 3</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                <a href='#'>Watch Now</a>
-            </div>
-        """, unsafe_allow_html=True)
-
-    st.write("</div>", unsafe_allow_html=True)
-
-else:
-    # Add upload form
-    st.markdown("## Upload a video")
-    uploaded_file = st.file_uploader("Choose an MP4 video", type="mp4")
-
-    # Perform emotion recognition analysis on the uploaded video and display the results
-    if uploaded_file:
-        result_container = st.empty()
-
-        # Show a loading message while we process the video
-        with st.spinner('Processing the video...'):
-            # Perform emotion recognition analysis here and get the results
-            # Add code here to perform the analysis
-            results = {
-                'happy': 0.2,
-                'sad': 0.3,
-                'angry': 0.4,
-                'neutral': 0.1
-            }
-
-        # Display the results
-        result_container.markdown("## Emotion Recognition Results")
-        for emotion, score in results.items():
-            result_container.write(f"- **{emotion.title()}**: {score:.2f}")
-
-
-# Below code is to hide default hamburger menu and streamlit footer
-
-# hide_streamlit_style = """
-#             <style>
-#             #MainMenu {visibility: hidden;}
-#             footer {visibility: hidden;}
-#             </style>
-#             """
-# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    
